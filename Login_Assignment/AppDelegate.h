7ffbb67671c8413499203d30adad72a37a692de4
Abhijeetk <abhijeetk@abhijeet-mac.local>
2018-07-30 15:06:05 +0530

    Initial Commit

---
+++ Login_Assignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Login_Assignment
+//
+//  Created by Abhijeetk on 7/30/18.
+//  Copyright © 2018 Abhijeetk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Login_Assignment/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Login_Assignment
+//
+//  Created by Abhijeetk on 7/30/18.
+//  Copyright © 2018 Abhijeetk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
